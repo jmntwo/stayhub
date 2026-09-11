dad3479d231b0eb254a308e2c23c1f2561b16a3c
Jimin Yun <yunjimin02@gmail.com>
2026-09-12 00:25:57 +0900

    Initial commit

---
+++ src/main/java/com/example/stayhub/StayhubApplication.java
@@ -0,0 +1,13 @@
+package com.example.stayhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayhubApplication.class, args);
+	}
+
+}
